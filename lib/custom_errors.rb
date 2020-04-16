a886fdeaaec7a148af2b3da43812f844e1929388
Yakura Marzett <marzettyakura@yahoo.com>
2020-04-16 17:56:25 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,16 +1,16 @@
 class Person
   ...
- 
+
   def get_married(person)
     self.partner = person
     person.partner = self
   end
- 
+
   # Add the following two lines:
   class PartnerError < StandardError
   end
 end
- 
+
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
